3d783f42a1cdb025390dbc8fbd47db84fb448c2f
Ahmad Safari <ahmadsafari118128@gmail.com>
2025-10-11 17:46:29 +0330

    config: change next.config

--- apps/docs/next.config.ts
+++ apps/docs/next.config.ts
@@ -1,12 +1,8 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
+  reactStrictMode: true,
+  output: "standalone",
 };
 
 export default nextConfig;
